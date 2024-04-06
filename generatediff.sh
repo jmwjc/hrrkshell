0070127196c93a48fcc5dfba50991d096c495bfe
WuJC <jcwu@hqu.edu.cn>
2024-04-06 09:14:26 +0800

    revised 1

--- generatediff.sh
+++
@@ -1,8 +0,0 @@
-for filename in $(ls .)
-    if test -z $(string match -r '\S+[^diff,^v0,^backup,^response][.]tex' $filename)
-    else
-        echo $filename
-        set name $(string split . $filename)[1]
-        latexdiff --math-markup=1 $PWD/$(string join '' $name "_v0.tex") $PWD/$filename > $PWD/$(string join '' $name "_diff.tex")
-    end
-end
